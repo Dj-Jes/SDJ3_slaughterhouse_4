1d07170e418891b989f6900eb8e25d54a26a3b48
Dj-Jes <95280143+Dj-Jes@users.noreply.github.com>
2022-11-27 22:45:36 +0100

    slaughterhouse assignment4

---
+++ SlaughterHouse-main/src/main/java/via/sdj3/slaughterhouse/SlaughterHouseApplication.java
@@ -0,0 +1,14 @@
+package via.sdj3.slaughterhouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class SlaughterHouseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SlaughterHouseApplication.class, args);
+    }
+
+}
